969a93e2c1a648c4312dafe1dd20434b24564008
Navid Hosseini <hosseini.navid82@gmail.com>
2026-04-28 10:57:21 +0330

    filters logic implemented in the selectors.py | documentlistcreateview refactored to plain apiview

--- backend/documents/selectors.py
+++ backend/documents/selectors.py
@@ -1,9 +1,19 @@
+import django_filters
 from .models import Document
 
+class DocumentFilter(django_filters.FilterSet):
+    search = django_filters.CharFilter(field_name='file_name', lookup_expr='icontains')
+    class Meta:
+        model = Document
+        fields = {
+            'file_type': ['exact'],
+            'status': ['exact'],
+            'file_name': ['icontains'],
+        }
 
-def document_list(*, user):
-    return Document.objects.filter(user=user)
-
+def document_list(*, user, filters=None):
+    qs = Document.objects.filter(user=user).order_by('-created_at')
+    return DocumentFilter(filters, queryset=qs).qs
 
 def document_get(*, user, document_id):
     from .services import DocumentNotFoundError

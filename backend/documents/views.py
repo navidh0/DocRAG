f149820c4d68863486eea20e8644241c0d53aa21
Navid Hosseini <hosseini.navid82@gmail.com>
2026-04-28 11:04:39 +0330

    apis refactored

--- backend/documents/views.py
+++ backend/documents/views.py
@@ -1,10 +1,9 @@
-from rest_framework import generics, filters, status
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.pagination import PageNumberPagination
-from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import (
     extend_schema, 
     OpenApiResponse, 
@@ -25,56 +24,6 @@ from .selectors import(
     document_get,
 )
 
-# @extend_schema(
-#     methods=['POST'],
-#     request={
-#         'multipart/form-data': {
-#             'type': 'object',
-#             'properties': {
-#                 'file': {
-#                     'type': 'string',
-#                     'format': 'binary',
-#                 },
-#             },
-#             'required': ['file'],
-#         }
-#     },
-#     responses={201: DocumentOutputSerializer},
-# )
-# class DocumentListCreateView(generics.ListCreateAPIView):
-#     serializer_class = DocumentOutputSerializer
-#     permission_classes = [IsAuthenticated]
-#     parser_classes = [MultiPartParser, FormParser]
-#     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-#     filterset_fields = ['file_type', 'status']
-#     search_fields = ['file_name']
-#     ordering_fields = ['created_at', 'file_name']
-#     ordering = ['-created_at']
-
-#     def get_queryset(self):
-#         return document_list(user=self.request.user)
-    
-#     def create(self, request, *args, **kwargs):
-#         input_ser = DocumentUploadInputSerializer(data=request.data)
-#         input_ser.is_valid(raise_exception=True)
-#         doc = CreateDocumentService.execute(
-#             user=request.user,
-#             validated_data=input_ser.validated_data,
-#         )
-#         output_ser = DocumentOutputSerializer(doc)
-#         return Response(output_ser.data, status=status.HTTP_201_CREATED)
-
-# @extend_schema(
-#     methods=['DELETE'],
-#     responses={
-#         200: OpenApiResponse(description="Document deleted successfully"),
-#         404: OpenApiResponse(description="Document not found"),
-#     },
-# )
-# @extend_schema(
-#     methods=['GET'],
-#     responses={200: DocumentOutputSerializer},
-# )
 class DocumentListCreateView(APIView):
     parser_classes = [MultiPartParser, FormParser]
 
@@ -84,7 +33,7 @@ class DocumentListCreateView(APIView):
         paginator = PageNumberPagination()
         paginated_qs = paginator.paginate_queryset(documents, request, view=self)
         output = DocumentOutputSerializer(paginated_qs, many=True)
-        
+
         return Response(
         paginator.get_paginated_response(output.data).data,
         status=status.HTTP_200_OK
@@ -110,24 +59,27 @@ class DocumentListCreateView(APIView):
         output = DocumentOutputSerializer(doc)
         return Response(output.data, status=status.HTTP_201_CREATED)
 
-class DocumentRetrieveDestroyView(generics.RetrieveDestroyAPIView):
-    serializer_class = DocumentOutputSerializer
-    permission_classes = [IsAuthenticated]
-    lookup_field = 'id'
+class DocumentRetrieveDestroyView(APIView):
 
-    def get_queryset(self):
-        return document_list(user=self.request.user)
+    @extend_schema(responses={200: DocumentOutputSerializer})
+    def get(self, request, id):
+        doc = document_get(user=request.user, document_id=id)
+        output = DocumentOutputSerializer(doc)
+        return Response(output.data, status=status.HTTP_200_OK)
 
-    def destroy(self, request, *args, **kwargs):
-        data = DeleteDocumentService.execute(
-            user=request.user,
-            document_id=kwargs['id'],
+    @extend_schema(
+        responses={
+            200: OpenApiResponse(description="Document deleted successfully"),
+            404: OpenApiResponse(description="Document not found"),
+        }
+    )
+    def delete(self, request, id):
+        data = DeleteDocumentService.execute(user=request.user, document_id=id)
+        return Response(
+            {"message": "Document deleted successfully", "details": data},
+            status=status.HTTP_200_OK,
         )
-        return Response({
-            "message": "Document deleted successfully",
-            "details": data,
-        }, status=status.HTTP_200_OK)
-
+    
 class DocumentStatusView(APIView):
     permission_classes = [IsAuthenticated]
 

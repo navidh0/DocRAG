00c19d7cae0ccf1f9918c7ae2ab186af3b589957
Navid Hosseini <hosseini.navid82@gmail.com>
2026-05-05 16:37:35 +0330

    signals.py refactored

--- backend/qa/apps.py
+++ backend/qa/apps.py
@@ -1,9 +1,5 @@
 from django.apps import AppConfig
 
-
 class QaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'qa'
-
-    def ready(self):
-        import qa.signals
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "qa"
